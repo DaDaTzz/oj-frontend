9c5fa565926544c45325585d9979cb3c1fe9363f
Da <1349190697@qq.com>
2024-06-12 15:48:06 +0800

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="home">
     主页
+    <MdEditor :value="value" :handleChange="onChange"/>
+    <CodeEditor />
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+
+import {defineComponent, ref} from "vue";
+import MdEditor from "@/views/MdEditor.vue";
+import CodeEditor from "@/views/user/CodeEditor.vue";
+
+defineComponent({
+  components: {MdEditor}
+})
+
+const value = ref('')
+const onChange = (v: string) => {
+  value.value = v
+}
+
 
 </script>
